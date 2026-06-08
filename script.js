b880c99b361b18a1ea9ac512f74e30fc5d63e7f4
SirArleayous <ah7063108268@gmail.com>
2026-06-08 08:48:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const themeBtn = document.getElementById("themeBtn");
+themeBtn.addEventListener("click", () => {
+document.body.classList.toggle("light");
+if(document.body.classList.contains("light")){
+themeBtn.innerHTML = "☀️";
+}else{
+themeBtn.innerHTML = "🌙";
+}
+});
